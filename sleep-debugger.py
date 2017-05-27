72590ec735003c5b70c8acec0a0d0b26c897ef60
Robert Kaye <rob@musicbrainz.org>
2017-05-27 15:21:51 +0200

    Finish implementing main loop

--- sleep-debugger.py
+++ sleep-debugger.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from atexit import register
-from time import sleep
+from time import sleep, time
 from sleepdebugger.process_lock import ProcessLock
 from sleepdebugger.accelerometer import AccelerometerReader
 from sleepdebugger.light_sensor import LightSensorReader
@@ -22,23 +22,34 @@ class SleepDebugger(object):
         self.accel = None
         if config.ACCEL_SENSOR:
             self.accel = AccelerometerReader("accelerometer", config.ACCEL_SENSOR)
+            self.accel_time = 0
 
         self.light = None
         if config.LIGHT_SENSOR:
             self.light = LightSensorReader("light_sensor", config.LIGHT_SENSOR)
-sefl
+            self.light_time = 0
 
         self.temphum = None
         if config.TEMP_HUM_SENSOR:
             self.temphum = TempHumSensorReader("temp_hum_sensor", config.TEMP_HUM_SENSOR)
+            self.temphum_time = 0
 
     def run(self):
         while True:
-            if self.light:
+            if self.light and (not self.light_time or time() >= self.light_time):
                 self.light.read()
-            if self.temphum:
+                self.light_time = time() + config.LIGHT_SENSOR_SAMPLE_PERIOD
+
+            if self.temphum and (not self.temphum_time or time() >= self.temphum_time):
                 self.temphum.read()
-            sleep(config.LIGHT_SENSOR_SAMPLE_PERIOD)
+                self.temphum_time = time() + config.TEMP_HUM_SAMPLE_PERIOD
+
+            if self.accel and (not self.accel_time or time() >- self.accel_time):
+                self.accel.read()
+                self.accel_time = time() + (1.0 / config.ACCEL_SAMPLES_PER_SECOND)
+
+            # Improve this
+            sleep(.0005)
 
 
 if __name__ == "__main__":
