922e89fde1be8bd31ff1708c662eb00942c23553
Robert Kaye <rob@musicbrainz.org>
2017-05-29 19:53:22 +0200

    Report every 10 seconds, regardless

--- sleepdebugger/radar_sensor.py
+++ sleepdebugger/radar_sensor.py
@@ -3,15 +3,18 @@ from sleepdebugger.reader import Reader
 
 class RadarSensorReader(Reader):
 
+    report_interval = 10
     def __init__(self, type, model):
         super(RadarSensorReader, self).__init__(type, model)
 
         self.value = -1
+        self.last_reported = 0
+
 
     def read(self):
         pt = self.sensor.read()
-        print pt
-        if pt != self.value:
+        if pt != self.value or (time() - self.last_reported) > self.report_interval:
             self._save_data("radar", { 'radar' : pt })
+            self.last_reported = time()
 
         self.value = pt
