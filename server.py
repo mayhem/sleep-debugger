c00e8c19ac37f0427b2234c2a9a034068b39d30d
Robert Kaye <rob@musicbrainz.org>
2017-05-31 23:12:02 +0200

    Implemeted a simple data export feature that lets the user download CSV files

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
+from dateutil.parser import parse
+from math import modf
+from time import mktime
+import csv
+import StringIO
 import sleepdebugger.config as config
-from flask import Flask, request, flash, url_for, render_template, redirect
+from flask import Flask, request, flash, url_for, render_template, redirect, Response
 from flask_debugtoolbar import DebugToolbarExtension
 from influxdb import InfluxDBClient
 
@@ -44,6 +49,54 @@ def record():
             flash("Entry was not recorded. Influx write error: %s" % str(err))
 
     return render_template("index")
+
+@app.route("/export", methods=['GET'])
+def export():
+    return render_template("export")
+
+@app.route("/export", methods=['POST'])
+def export_data():
+    start = request.form['start']
+    end = request.form['end']
+
+    try:
+        start = parse(start)
+    except ValueError:
+        flash("Cannot decipher format of start time.")
+        return render_template("export", start=request.form['start'], end=request.form['end'])
+
+    try:
+        end = parse(end)
+    except ValueError:
+        flash("Cannot decipher format of end time.")
+        return render_template("export", start=request.form['start'], end=request.form['end'])
+    
+    try:
+        influx = InfluxDBClient(config.INFLUX_HOST, config.INFLUX_PORT, config.INFLUX_USER, config.INFLUX_PASSWD, config.INFLUX_DB)
+    except Exception as err:
+        flash("Cannot connect to DB.")
+        return render_template("export", start=request.form['start'], end=request.form['end'])
+
+    decimal, integer = modf(mktime(start.timetuple()))
+    start = "%d" % integer + "%09d" % (decimal * 1000000000)
+
+    decimal, integer = modf(mktime(end.timetuple()))
+    end = "%d" % integer + "%09d" % (decimal * 1000000000)
+    
+    query = "SELECT mag FROM sleep WHERE time >= %s and time <= %s" % (start, end)
+    try:
+        results = influx.query(query)
+    except Exception as e:
+        flash("Cannot query influx: %s" % str(e))
+        return render_template("export", start=request.form['start'], end=request.form['end'])
+
+    si = StringIO.StringIO()
+    cw = csv.writer(si)
+    for result in results.get_points(measurement="sleep"):
+        print result 
+        cw.writerow([ result['time'], result['mag'] ])
+
+    return Response(si.getvalue().strip('\r\n'), mimetype='text/csv')
 
 if __name__ == "__main__":
     app.run(host=config.HOST, port=config.PORT, threaded=True)
